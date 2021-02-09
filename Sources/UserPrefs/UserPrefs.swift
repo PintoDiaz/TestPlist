f1579f6abef6601482844cc6477521903dba63ab
Roger Pintó <roger.pinto@sage.com>
2021-02-09 15:24:57 +0100

    Basic setup 3

--- Sources/UserPrefs/UserPrefs.swift
+++ Sources/UserPrefs/UserPrefs.swift
@@ -3,6 +3,10 @@ import UIKit
 public struct UserPrefs {
     var text = "Hello, World!"
 
+    public init() {
+        getProperties()
+    }
+
     public func getProperties() {
         let properties = UserDefaults.standard.dictionaryRepresentation().values
         print(properties)
