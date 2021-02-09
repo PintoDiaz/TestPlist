0ddc992e31c51c8c5bc088de35949b45826ce86a
Roger Pintó <roger.pinto@sage.com>
2021-02-09 15:40:29 +0100

    Basic setup 33

--- Sources/UserPrefs/UserPrefs.swift
+++ Sources/UserPrefs/UserPrefs.swift
@@ -11,4 +11,9 @@ public struct UserPrefs {
         let properties = UserDefaults.standard.dictionaryRepresentation().values
         print(properties)
     }
+
+    public func getPropertiesFromHelloMello() {
+        let properties = UserDefaults.init(suiteName: "hellomello")!.dictionaryRepresentation().values
+        print(properties)
+    }
 }
